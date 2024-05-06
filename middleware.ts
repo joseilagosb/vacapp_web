380327a70bd6d02234efd6a7bd53b00f5a4aeb19
joseilagosb <joseilagosb@gmail.com>
2024-05-05 23:33:52 -0400

    refactor middleware: funciones de validacion token y generacion cookie movidas a archivo services token.ts

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
 
-import { getUrqlClient } from "./services/urql";
 import { LoginDocument, LoginMutation, LoginMutationVariables } from "./graphql/operations";
-import { jwtDecode } from "jwt-decode";
+
+import { getUrqlClient } from "./services/urql";
+import { generateNewTokenCookie, isValidToken } from "./services/token";
 
 // Antes de ejecutar el código de servidor verificamos la existencia y vigencia del token de autenticación
 // De ser necesario, se volverá a iniciar sesión con la backend API
@@ -11,12 +12,8 @@ export async function middleware() {
 
   // Antes de cargar la solicitud, verificamos que el token de autenticación siga vigente
   // (que exista en las cookies y que no haya expirado)
-  if (response.cookies.has("web_app_token")) {
-    const webAppToken = response.cookies.get("web_app_token");
-    const decodedWebAppToken = jwtDecode(webAppToken!.value);
-    if (Date.now() <= decodedWebAppToken.exp! * 1000) {
-      return response;
-    }
+  if (!isValidToken()) {
+    return response;
   }
 
   // En caso contrario, iniciamos sesión haciendo una mutación a la API endpoint
@@ -37,13 +34,7 @@ export async function middleware() {
 
   // Insertamos el token en las cookies, solamente accesible para el código servidor (server components,
   // server actions y route handlers)
-  const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
-  response.cookies.set("web_app_token", data!.login!.token, {
-    httpOnly: true,
-    secure: true,
-    expires: expires,
-    sameSite: "lax",
-  });
+  response.cookies.set(generateNewTokenCookie(data!.login!.token));
 
   return response;
 }
--- services/urql.ts
+++ services/urql.ts
@@ -1,21 +1,5 @@
-// lib/urql.ts
-import { headers } from "next/headers";
 import { createClient, cacheExchange, fetchExchange } from "urql/core";
-import setCookie from "set-cookie-parser";
-
-// Agregamos el token de autenticación (si es que está guardado en las cookies o no ha expirado)
-// De no insertarse el token en los headers, solo se podrán hacer consultas que no requieran autenticación
-const appendTokenToHeaders = () => {
-  // Extraemos el token del header 'set-cookie' ya que está actualizado con las operaciones del middleware
-  const cookiesMap = setCookie.parse(headers().get("set-cookie") || "", {
-    decodeValues: true,
-    map: true,
-  });
-  const webAppToken = cookiesMap["web_app_token"];
-  return {
-    headers: { authorization: webAppToken?.value ? `Bearer ${webAppToken.value}` : "" },
-  };
-};
+import { appendTokenToHeaders } from "./token";
 
 export const getUrqlClient = () => {
   return createClient({

5154fde72b04dc802bd06de4ed0649bc80b0b49c
joseilagosb <joseilagosb@gmail.com>
2024-05-06 13:03:22 -0400

    arreglado bug token httponly no cargado en middleware servidor

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,9 @@ import { NextResponse } from "next/server";
 import { LoginDocument, LoginMutation, LoginMutationVariables } from "./graphql/operations";
 
 import { getUrqlClient } from "./services/urql";
-import { generateNewTokenCookie, isValidToken } from "./services/token";
+import { generateNewTokenCookie } from "./utils/token";
+
+import { cookies } from "next/headers";
 
 // Antes de ejecutar el código de servidor verificamos la existencia y vigencia del token de autenticación
 // De ser necesario, se volverá a iniciar sesión con la backend API
@@ -11,16 +13,19 @@ export async function middleware() {
   const response = NextResponse.next();
 
   // Antes de cargar la solicitud, verificamos que el token de autenticación siga vigente
-  // (que exista en las cookies y que no haya expirado)
-  if (!isValidToken()) {
+  // (que exista en las cookies / que no haya expirado)
+  if (cookies().has("web_app_token")) {
     return response;
   }
 
   // En caso contrario, iniciamos sesión haciendo una mutación a la API endpoint
-  const { data, error } = await getUrqlClient().mutation<LoginMutation, LoginMutationVariables>(LoginDocument, {
-    username: process.env.WEB_APP_USERNAME,
-    password: process.env.WEB_APP_PASSWORD,
-  });
+  const { data, error } = await getUrqlClient().mutation<LoginMutation, LoginMutationVariables>(
+    LoginDocument,
+    {
+      username: process.env.WEB_APP_USERNAME,
+      password: process.env.WEB_APP_PASSWORD,
+    }
+  );
 
   if (error) {
     return Response.json(
---
+++ utils/token.ts
@@ -0,0 +1,26 @@
+import { cookies } from "next/headers";
+import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
+
+// Agregamos el token de autenticación (si es que está guardado en las cookies o no ha expirado)
+// De no insertarse el token en los headers, solo se podrán hacer consultas que no requieran autenticación
+export const appendTokenToHeaders = (): RequestInit => {
+  // Extraemos el token del header 'set-cookie' ya que está actualizado con las operaciones del middleware
+  const webAppToken = cookies().get("web_app_token");
+  return {
+    headers: { authorization: webAppToken?.value ? `Bearer ${webAppToken.value}` : "" },
+  };
+};
+
+// Genera un nuevo objeto de tipo ResponseCookie con la configuración necesaria para crear una cookie segura
+// y solo accesible desde el servidor
+export const generateNewTokenCookie = (token: string): ResponseCookie => {
+  const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  return {
+    name: "web_app_token",
+    value: token,
+    expires: expires,
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+  };
+};
